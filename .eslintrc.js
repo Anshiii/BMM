ccf56723c991402d4a0af72e81abfc6fb14309ed
Anshi <bisexua1@foxmail.com>
2017-10-25 20:25:02 +0800

    初始化 eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,37 @@
 module.exports = {
-    "extends": "airbnb"
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    // "extends":['plugin:react/recommended'],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "parser": "babel-eslint",
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
 };
